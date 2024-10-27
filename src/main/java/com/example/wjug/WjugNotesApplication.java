a114f1c11213c323d5484a84105f1ef00c815ef2
perfringis <kopko.mateusz@gmail.com>
2024-10-27 12:47:26 +0100

    init commit

---
+++ src/main/java/com/example/wjug/WjugNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.wjug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjugNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WjugNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wjug/WjugNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wjug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjugNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
